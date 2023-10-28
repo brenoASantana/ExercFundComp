e3ab8c6c5751623ee9de88814f9f32ff3605d389
Breno Santana <contatobrenosantana@outlook.com>
2023-10-28 06:56:12 -0300

    Organização

--- exSlide1710.py
+++ exSlide1710.py
@@ -1,9 +1,13 @@
 import math
 
-n = int(input("Informe um número: "))
+n = int(input("Informe um número positivo: "))
 
-if n <= 0:
-    print("Informe um número maior que zero.")
+s = 0
 
-for cont in range(1, n+1):
-    sqrt = math.sqrt(n)
+while n < 0:
+    n = int(input("Informe um número POSITIVO: "))
+
+for cont in range(1, n + 1):
+    s += math.sqrt(n)
+
+print(f"O valor é: {s}")
--- p1.py
+++ p1.py
@@ -6,14 +6,13 @@ while n % 2 == 0:
     n = float(input("Informe o valor IMPAR de N: "))
 
 i = 1
-for cont in range(1, n+1, 2):
-
-## O que me dificultou foi achar um jeito de saber se devo somar ou subtratir os valores e ainda manter o mesmo valor de n
+for cont in range(1, n + 1, 2):
+    ## O que me dificultou foi achar um jeito de saber se devo somar ou subtratir os valores e ainda manter o mesmo valor de n
 
     if i % 2 == 0:
-        s += (x**n)/n
+        s += (x**n) / n
     else:
-        s -= (x**n)/n
+        s -= (x**n) / n
 
     i += 1
 
