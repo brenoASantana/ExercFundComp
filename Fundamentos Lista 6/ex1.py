82de577ebb8057316894e956356059577287eb28
Breno Santana <contatobrenosantana@outlook.com>
2023-11-21 16:33:11 -0300

    Exercício 1 Resolvido

--- Fundamentos Lista 6/ex1.py
+++ Fundamentos Lista 6/ex1.py
@@ -1,13 +1,13 @@
-vetpos = [0] * 10
-vetneg = [0] * 10
+vetpos = []
+vetneg = []
 
 for i in range(10):
     n = int(input("Informe um valor:\n"))
 
     if n > 0:
-        vetpos[i] = n
+        vetpos += [n]
     if n < 0:
-        vetneg[i] = n
+        vetneg += [n]
 
 print(f"Vetor Positivo: {vetpos}")
 print(f"Vetor Negativo: {vetneg}")
