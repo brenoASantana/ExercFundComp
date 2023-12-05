f0e78cbb5c9a0059447b1f1f2f0d8e8643967b84
Breno Santana <contatobrenosantana@outlook.com>
2023-12-05 12:44:21 -0300

    Exercício do menu feito

---
+++ funcaoFazerVetorMatriz.py
@@ -0,0 +1,13 @@
+def fazerVetor(tamVet):
+    return
+
+
+def fazerMatriz(linMat, colMat):
+    return
+
+
+tamVet = int(input("Informe o tamanho desejado do vetor: "))
+linMat = int(input("Informe o tamanho desejado da linha da matriz: "))
+colMat = int(input("Informe o tamanho desejado da coluna da matriz: "))
+fazerVetor(tamVet)
+fazerMatriz(linMat, colMat)
